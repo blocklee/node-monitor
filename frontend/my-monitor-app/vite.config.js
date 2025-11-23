efb3fa010c6c828131c5716919b37901087addf1
blocklee <block.lee121@gmail.com>
2025-11-23 14:13:46 +0800

    del vite.svg

--- frontend/my-monitor-app/vite.config.js
+++ frontend/my-monitor-app/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/node-monitor/',  // 替换成你的仓库名
+  base: '/node-monitor/',  // GitHub Pages 仓库名
   server: {
     port: 5173,
     host: true
